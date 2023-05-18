e23c95772cd42c57eb7788518d4ebb2922507d20
Sourcery AI <>
2023-05-18 05:22:41 +0000

    'Refactored by Sourcery'

--- Il2CppInspector.Common/Outputs/ScriptResources/Targets/IDA.py
+++ Il2CppInspector.Common/Outputs/ScriptResources/Targets/IDA.py
@@ -4,7 +4,7 @@ import idaapi
 def SetName(addr, name):
 	ret = idc.set_name(addr, name, SN_NOWARN | SN_NOCHECK)
 	if ret == 0:
-		new_name = name + '_' + str(addr)
+		new_name = f'{name}_{str(addr)}'
 		ret = idc.set_name(addr, new_name, SN_NOWARN | SN_NOCHECK)
 
 def MakeFunction(start, name=None, addrMax=None):
@@ -29,16 +29,14 @@ def SetFunctionType(addr, sig):
   SetType(addr, sig)
 
 def SetType(addr, cppType):
-  if not cppType.endswith(';'):
-    cppType += ';'
-  tinfo = idc.parse_decl(cppType,idaapi.PT_RAWARGS)
-  ret = None
-  if not(tinfo is None):
-    ret = idc.apply_type(addr,tinfo)
-  if ret is None:
-    ret = idc.SetType(addr, cppType)
-  if ret is None:
-    print('SetType(0x%x, %r) failed!' % (addr, cppType))
+	if not cppType.endswith(';'):
+	  cppType += ';'
+	tinfo = idc.parse_decl(cppType,idaapi.PT_RAWARGS)
+	ret = idc.apply_type(addr,tinfo) if tinfo is not None else None
+	if ret is None:
+	  ret = idc.SetType(addr, cppType)
+	if ret is None:
+	  print('SetType(0x%x, %r) failed!' % (addr, cppType))
 
 def SetComment(addr, text):
   idc.set_cmt(addr, text, 1)
@@ -50,7 +48,7 @@ def CustomInitializer():
 	print('Processing Types')
 
 	original_macros = ida_typeinf.get_c_macros()
-	ida_typeinf.set_c_macros(original_macros + ";_IDA_=1")
+	ida_typeinf.set_c_macros(f"{original_macros};_IDA_=1")
 	idc.parse_decls(os.path.join(GetScriptDirectory(), "%TYPE_HEADER_RELATIVE_PATH%"), idc.PT_FILE)
 	ida_typeinf.set_c_macros(original_macros)
 
